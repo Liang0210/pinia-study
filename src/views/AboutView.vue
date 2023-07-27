2a93cb682be8a2c38dfabce1afe8ce821bea7b2f
liangye <liangye@eccom.com.cn>
2023-07-27 16:01:53 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {useCounterStore} from '../stores/counter';
+const countStore=useCounterStore()
+</script>
+<template>
+  <div class="about">
+  <h1>count : {{ countStore.count }}</h1>
+  <h1>2*count: {{ countStore.doubleCount }}</h1>
+  <button @click="countStore.increment()">increment</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
